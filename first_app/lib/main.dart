3fada080365ae8613fd83160267147839688d7cf
Hernan <zerovirus23@gmail.com>
2019-10-29 22:14:50 -0500

    Stateful widget working (increasing the text size of buttons)

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TextNoLayout());
+void main() => runApp(SurveyApp());
 
-class TextNoLayout extends StatelessWidget {
+class SurveyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SurveyAppState();
+  }
+}
+
+class SurveyAppState extends State<SurveyApp> {
+  double currentSize = 10;
+
+  increaseTextSize(int size) {
+    return () {
+      setState(() {
+        currentSize += size;
+      });
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Text('This is just a Hello World!!!',
-            textAlign: TextAlign.center,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(fontWeight: FontWeight.bold)));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('This is just a Hello World!!!'),
+        ),
+        body: Row(
+          children: [
+            RaisedButton(
+              onPressed: increaseTextSize(1),
+              child: Text('Question 1', style: TextStyle(fontSize: currentSize)),
+            ),
+            RaisedButton(
+              onPressed: increaseTextSize(2),
+              child: Text('Question 2', style: TextStyle(fontSize: currentSize)),
+            ),
+            RaisedButton(
+              onPressed: increaseTextSize(3),
+              child: Text('Question 3', style: TextStyle(fontSize: currentSize)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
